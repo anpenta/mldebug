2aa8b5d08a864e2e614ca30c01a07233e0c48468
Andreas Pentaliotis <40018182+anpenta@users.noreply.github.com>
2026-04-30 17:03:54 +0300

    Refactor rename check functions to show intent (#11)

--- tests/checks/test_ks.py
+++ tests/checks/test_ks.py
@@ -1,14 +1,14 @@
-from mldebug.checks.ks import run_ks_test_check
+from mldebug.checks.ks import run_numeric_ks_test_check
 from tests.factories.data import generate_normal_data
 
 
-def test_ks_detects_shift() -> None:
+def test_run_numeric_ks_test_check_detects_shift() -> None:
     feature = "feature_1"
 
     reference = generate_normal_data()
     current = generate_normal_data(mean=1, std=1)
 
-    issue = run_ks_test_check(
+    issue = run_numeric_ks_test_check(
         feature=feature,
         reference=reference,
         current=current,
@@ -20,13 +20,13 @@ def test_ks_detects_shift() -> None:
     assert issue.feature == feature
 
 
-def test_ks_no_detection_when_stable() -> None:
+def test_run_numeric_ks_test_check_no_detection_when_stable() -> None:
     feature = "feature_1"
 
     reference = generate_normal_data()
     current = generate_normal_data(mean=0.05)
 
-    issue = run_ks_test_check(
+    issue = run_numeric_ks_test_check(
         feature=feature,
         reference=reference,
         current=current,
