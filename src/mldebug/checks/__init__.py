c856e0115c71224d5715f6aa1fb28eddc14e2988
Andreas Pentaliotis <40018182+anpenta@users.noreply.github.com>
2026-05-07 18:57:55 +0300

    feat: simplify inits

--- src/mldebug/checks/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Check implementations for mldebug.
-
-Organized by feature type (numeric, categorical, etc). These are internal implementations used via the registry.
-"""
-
-__all__ = []
--- src/mldebug/checks/numeric/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Numeric feature checks for mldebug.
-
-Implements statistical and data quality checks for numeric features.
-"""
-
-__all__ = []
